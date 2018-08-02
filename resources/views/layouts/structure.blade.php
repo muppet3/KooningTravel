28dcf28f524c0b59bbfff69eee256ae88e20ea7b
Jonas <thmixsadecv@hotmail.com>
2018-08-02 12:45:35 -0500

    ....

---
+++ resources/views/layouts/structure.blade.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="es">
+
+  <head>
+
+        <!--Meta tags-->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+      <meta name="google-site-verification" content="JsJB8p487oyOxvtEpsDjpuQZJ7GeOMe22sQr39OBTQ0" >
+      <meta name="description" content="Hoteles, traslados y tours en Cancún, y la Riviera Maya. Elige tus vacaciones entre miles de hoteles y destinos.">
+      <meta name="author" content="Ing. Jonas Santiz L., Lic. Alan E. Gonzalez, Ing. Fernando Morales R. || Kooning Travel 2018 ">
+      <link rel="shortcut icon" href="https://kooningtravel.com/images/favicon.png" type="image/png">
+      <title>Hoteles, traslados y tours - Kooning Travel - 01 800 890 8974</title>
+      
+       <!--Recursos JS -->
+      <script language="javascript" type="text/javascript" src="/js/jquery.min.js"></script> 
+      
+
+      <?php $path = $_SERVER['REQUEST_URI']; ?>
+
+      <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+          ga('create', 'UA-92499808-1', 'auto');
+          ga('send', 'pageview');
+      </script>
+
+      <!-- Facebook Pixel Code -->
+      <script>
+          !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+          n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+          document,'script','https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '695241874015412'); // Insert your pixel ID here.
+          fbq('track', 'PageView');
+      </script>
+
+
+    <!-- Hotjar Tracking Code for https://kooningtravel.com/ -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:913728,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
+
+      <!--Redireccion-->
+      <script type="text/javascript">
+            if (screen.width < 1200) {  window.location = "http://192.168.10.10/"; } 
+      </script>
+
+      <!-- Google Tag Manager -->
+        <script>
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T47XGRM');
+        </script>
+      <!-- End Google Tag Manager -->
+
+        <noscript>
+          <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=695241874015412&ev=PageView&noscript=1" alt="">
+        </noscript>
+
+
+      <!-- DO NOT MODIFY -->
+      <!-- End Facebook Pixel Code -->
+      <!-- Google Code para etiquetas de remarketing -->
+      <!--
+      Es posible que las etiquetas de remarketing todavía no estén asociadas a la información de identificación personal o que estén en páginas relacionadas con las categorías delicadas. Para obtener más información e instrucciones sobre cómo configurar la etiqueta, consulte https://google.com/ads/remarketingsetup.
+      -->
+      <script type="text/javascript">
+          var google_tag_params = {
+              travel_destid: 'REPLACE_WITH_VALUE',
+              travel_originid: 'REPLACE_WITH_VALUE',
+              travel_startdate: 'REPLACE_WITH_VALUE',
+              travel_enddate: 'REPLACE_WITH_VALUE',
+              travel_pagetype: 'REPLACE_WITH_VALUE',
+              travel_totalvalue: 'REPLACE_WITH_VALUE',
+          };
+
+          /* <![CDATA[ */
+          var google_conversion_id = 856961916;
+          var google_custom_params = window.google_tag_params;
+          var google_remarketing_only = true;
+          /* ]]> */
+
+      </script>
+
+    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script>
+    <noscript>
+        <div style="display:inline;">
+            <img height="1" width="1" style="border-style:none;" alt=" " src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/856961916/?guid=ON&amp;script=0">
+        </div>
+    </noscript>
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T47XGRM" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+
+    <script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script>
+    <script type="text/javascript">require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us16.list-manage.com","uuid":"982e50e16feb60f810399c614","lid":"fc48ed2a08"}) })</script>
+
+  </head>
+
+  <body>
+
+
+    <div class="main-desk" >
+      
+
+    </div>
+
+
+
+
+  <div style="display: none;" class='wait'>
+    <div class='loader'>
+      <div class='loader--dot'></div>
+      <div class='loader--dot'></div>
+      <div class='loader--dot'></div>
+      <div class='loader--dot'></div>
+      <div class='loader--dot'></div>
+      <div class='loader--dot'></div>
+      <div class='loader--text'></div>
+    </div>
+  </div>
+
+  <?php //include(ROOT.'/application/views/commons/footer.php'); ?>
+
+     <!--Recursos JS -->
+  <script language="javascript" type="text/javascript" src="/js/bootstrap.min.js"></script>
+  <script language="javascript" type="text/javascript" src="/js/select2.full.min.js"></script>
+  <script language="javascript" type="text/javascript" src="/js/caja-reserva2.js" ></script> 
+  <script language="javascript" type="text/javascript" src="/js/caja/main.js" ></script> 
+  <script language="javascript" type="text/javascript" src="/js/general.js" ></script> 
+
+  <!--Recursos CSS -->
+  <link type="text/css" rel="stylesheet" media="screen" href="/css/font-awesome.min.css">
+  <link type="text/css" rel="stylesheet" media="screen" href="/bower_components/ihover/src/ihover.css">
+  <link type="text/css" rel="stylesheet" media="screen" href="/css/select2.min.css" />
+  <link type="text/css" rel="stylesheet" media="screen" href="/css/estilo.css">
+  <link type="text/css" rel="stylesheet" media="screen" href="/css/bootstrap.min.css" >
+  <link type="text/css" rel="stylesheet" media="screen" href="/css/caja/box.css" />
+  <link type="text/css" rel="stylesheet" media="screen" href="https://fonts.googleapis.com/css?family=Oswald:300,700" >      
+
+  </body>
+</html>
