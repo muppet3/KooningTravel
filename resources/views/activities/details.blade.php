c426490012b8d61258c14b9dd856bab8ea78e9e9
Jonas <jlopez90@live.com.mx>
2018-08-24 16:31:05 -0500

    ...

--- resources/views/activities/details.blade.php
+++ resources/views/activities/details.blade.php
@@ -100,7 +100,7 @@
         </div>
         <label class="tx16">Precios sujetos a cambios sin previo aviso. Aplican restricciones.</label>
       </form>
-      <div style="background:#009626" class="infoinf">
+      <div style="background:{{$activity->background}}" class="infoinf">
         <div class="hreft1">
           <div class="monya">
             <span class="ad" >Precio Adulto</span>
@@ -111,8 +111,6 @@
             <p class="cost nin">$ 8787</p>
           </div>
         </div>
-        <div  style="display: none; background:green; "> Ahorra hasta 12 meses sin <br>
-          intereses. </div>
       </div>
     </div>
     <div class="recent-searchs mh clear menut2">
@@ -125,27 +123,37 @@
     </div>
   </div>
 </div>
+
+
+
 <div class="grey-content clear">
   <div class="wrapper">
     <section class="ticketing-hotels">
       <div class="main-title">
-        <h2 class="h2 titles">Entradas el parque!</h2>
+
+        <h2 class="h2 titles">Entradas el parque!</h2>	
+
       </div>
       <ul class="hotels-list">
-        <li class="item hotel">
-          <a data-fancybox data-type='iframe' data-src='https://kooningtravel.com/img/tour/terminosycondiciones.pdf' class="thumb"  href='javascript:void(0);'>
-            <div class="thumb-images"><img src="/img/tour/parques/Experiencias-Xcaret/Xcaret/Xcaret1.png" alt="el parque"></div>
-            <div class="info">
-              <h3 class="title">el parque<br>
-                <span class="category category-4CAT"><span class="category-name"></span></span></h3>
-            </div>
-          </a>
-          <a class="txtt" data-fancybox data-type='iframe' data-src='https://kooningtravel.com/img/tour/EntradaXcaretParque.pdf' href='javascript:void(0);' >
-            <p class="hotel-description"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua.</p>
-          </a>
-          <a data-fancybox data-type='iframe' data-src='https://kooningtravel.com/img/tour/terminosycondiciones.pdf' class="pdf"  href='javascript:void(0);' >Ver mas...</a>
-        </li>
+
+
+
+	@foreach($activity->tickets as $ticket)
+	        <li class="item hotel">
+	          <a data-fancybox data-type='iframe' data-src='https://kooningtravel.com/img/tour/terminosycondiciones.pdf' class="thumb"  href='javascript:void(0);'>
+	            <div class="thumb-images"><img src="{{$ticket->image}}" alt="el parque"></div>
+	            <div class="info">
+	              <h3 class="title">{{$ticket->name}}<br>
+	                <span class="category category-4CAT"><span class="category-name"></span></span></h3>
+	            </div>
+	          </a>
+	          <a class="txtt" data-fancybox data-type='iframe' data-src='https://kooningtravel.com/img/tour/EntradaXcaretParque.pdf' href='javascript:void(0);' >
+	            <p class="hotel-description">{{$ticket->description}}</p>
+	          </a>
+	          <a data-fancybox data-type='iframe' data-src='https://kooningtravel.com/img/tour/{{$ticket->content}}' class="pdf"  href='javascript:void(0);' >Ver mas...</a>
+	        </li>
+		@endforeach
+
         <li class="item hotel">
           <a target="_blank" class="thumb" href="https://kooningtravel.com/hotels/5051/hotel-xcaret-mexico?d=5051&sd=2018-05-01&ed=2018-05-05&r=1&r1a=2&r1k=0&r1k1a=0&r1k2a=0&r1k3a=0&r2a=0&r2k=0&r2k1a=0&r2k2a=0&r2k3a=0&r3a=0&r3k=0&r3k1a=0&r3k2a=0&r3k3a=0&r4a=0&r4k=0&r4k1a=0&r4k2a=0&r4k3a=0&r5a=0&r5k=0&r5k1a=0&r5k2a=0&r5k3a=0">
             <div class="thumb-images">
@@ -161,11 +169,14 @@
           </a>
           <p class="hotel-description">Hotel Xcaret M&eacute;xico es un destino enmarcado por la majestuosidad natural de la Riviera Maya y un dise&ntilde;o arquitect&oacute;nico que se sincroniza en armon&iacute;a con los elementos de esta tierra.</p>
         </li>
+
+
+
       </ul>
     </section>
     <div class="article">
       <section>
-        <h2>Informaci&oacute;n sobre parque el parque</h2>
+        <h2>Informaci&oacute;n sobre parque {{$activity->name}}</h2>
         <div class="clear">
           <div id="tabs">
             <ul>
@@ -178,22 +189,11 @@
             </ul>
             <div id="tabs-1">
               <div class="h1">Parque el parque!</div>
-              <h3 class="h3">Mapa y descripci&oacute;n parque el parque!</h3>
+              <h3 class="h3">Mapa y descripci&oacute;n parque {{$activity->name}}!</h3>
               <a  href="javascript:void(0);">
-                <img alt="Mapa del parque el parque!" class="left" src="/img/tour/parques/Experiencias-Xcaret/Xcaret/mapa-xcaret.png" style="margin-top: 0px; max-height: 250px;" />
+                <img alt="Mapa del parque el parque!" class="left" src="{{$activity->image}}" style="margin-top: 0px; max-height: 250px;" />
               </a>
-              <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo </p>
-              <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo </p>
-              <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo </p>
-              <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo </p>
+              <p>{{$activity->description}}</p>
               <iframe width="560" height="315" src="https://www.youtube.com/embed/8ofAxR3v3oU?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
             </div>
             <div id="tabs-2">
@@ -207,11 +207,11 @@
 </div>
 <div class="white-content full-content clear">
   <section class="mapas">
-    <h2 class="h2">Mapa Parque el parque!</h2>
+    <h2 class="h2">Mapa Parque {{$activity->name}}!</h2>
     <div class="mapa-box mapa-ciudad">
       <div class="mapa-bg">
         <div class="google-maps" id="map-canvas">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8883.815161584382!2d-87.12312858683697!3d20.5805355232904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x8042e81e921809a5!2sXcaret!5e0!3m2!1ses-419!2smx!4v1518549365644" width="100%" height="100%" frameborder="0" style="border:0; margin: 0px auto; float: none; display: inline-block;" allowfullscreen></iframe>
+          <iframe src="{{$activity->coordinates}}" width="100%" height="100%" frameborder="0" style="border:0; margin: 0px auto; float: none; display: inline-block;" allowfullscreen></iframe>
         </div>
       </div>
     </div>
@@ -220,67 +220,32 @@
 <!-- PARQUES EXPERIENCIAS XCARET 2018 -->
 <div class="grey-content clear" id="parque-de-atracciones-parques-tematicos">
   <div class="wrapper icon-tickets categoria-tickets clear  topt">
-    <h2 class="h2">Otros parques</h2>
+    <h2 class="h2">{{$activity->category->name}}</h2>
     <div class="carousel-wrapper">
       <div class="carousel center">
         <ul class="ticket-list clear">
+
+		@foreach ($activity->category->activities as $Activity)
           <li class="item">
             <div class="image-wrapp">
-              <a href="/Parques/Xcaret"><img src="/img/tour/parques/Experiencias-Xcaret/Xcaret/xcaret.png" alt=" " /></a>
+              <a href="{{Request::root()}}/{{strtolower($activity->category->type)}}/{{$Activity->name}}">
+              	<img src="{{$Activity->image}}" alt=" " /></a>
             </div>
             <h3>
-              <a href="/Parques/Xcaret">¡Xcaret, Orgullo de México!</a>
+              <a href="/Parques/Xcaret">{{$Activity->slogan}}</a>
             </h3>
             <p>
-              <a href="/Parques/Xcaret">Xcaret, Riviera Maya Quintana Roo, México.</a>
+              <a href="/Parques/Xcaret">{{$Activity->location}}</a>
             </p>
           </li>
-          <li class="item">
-            <div class="image-wrapp">
-              <a href="/Parques/Xcaret"><img src="/img/tour/parques/Experiencias-Xcaret/Xcaret/xcaret.png" alt=" " /></a>
-            </div>
-            <h3>
-              <a href="/Parques/Xcaret">¡Xcaret, Orgullo de México!</a>
-            </h3>
-            <p>
-              <a href="/Parques/Xcaret">Xcaret, Riviera Maya Quintana Roo, México.</a>
-            </p>
-          </li>
-          <li class="item">
-            <div class="image-wrapp">
-              <a href="/Parques/Xcaret"><img src="/img/tour/parques/Experiencias-Xcaret/Xcaret/xcaret.png" alt=" " /></a>
-            </div>
-            <h3>
-              <a href="/Parques/Xcaret">¡Xcaret, Orgullo de México!</a>
-            </h3>
-            <p>
-              <a href="/Parques/Xcaret">Xcaret, Riviera Maya Quintana Roo, México.</a>
-            </p>
-          </li>
-          <li class="item">
-            <div class="image-wrapp">
-              <a href="/Parques/Xcaret"><img src="/img/tour/parques/Experiencias-Xcaret/Xcaret/xcaret.png" alt=" " /></a>
-            </div>
-            <h3>
-              <a href="/Parques/Xcaret">¡Xcaret, Orgullo de México!</a>
-            </h3>
-            <p>
-              <a href="/Parques/Xcaret">Xcaret, Riviera Maya Quintana Roo, México.</a>
-            </p>
-          </li>
-          <li class="item">
-            <div class="image-wrapp">
-              <a href="/Parques/Xcaret"><img src="/img/tour/parques/Experiencias-Xcaret/Xcaret/xcaret.png" alt=" " /></a>
-            </div>
-            <h3>
-              <a href="/Parques/Xcaret">¡Xcaret, Orgullo de México!</a>
-            </h3>
-            <p>
-              <a href="/Parques/Xcaret">Xcaret, Riviera Maya Quintana Roo, México.</a>
-            </p>
-          </li>
+		@endforeach
+
         </ul>
       </div>
+
+			
+
+
       <a class="carousel-prev disabled"><span class="ir"></span></a>
       <a class="carousel-next"><span class="ir"></span></a>
     </div>
@@ -312,6 +277,9 @@ $(function () {
 		$("#search-tabs-entradas").tabs();			
 	});
 </script> 
+
+
+
 
 
 @stop
