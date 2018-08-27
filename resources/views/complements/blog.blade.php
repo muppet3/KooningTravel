ccb0e1d860c61db2ab45c26a1e1fec2cf850bb17
Jonas <jlopez90@live.com.mx>
2018-08-27 12:32:53 -0500

    ...

---
+++ resources/views/complements/blog.blade.php
@@ -0,0 +1,99 @@
+@extends('layouts/structure')
+@section('content')
+
+
+
+    <div class="blog" >
+
+    <div class="bloque cont1" >  
+        <div class="bl">          
+              <a class="tab1" href="javascript:void(0);">México | Cancún, Quintana Roo..</a>              
+              <a class="tab2" href="https://kooningtravel.com/blog/Los-Mejores-Hoteles-en-Cancun-para-Viajar-en-Familia">Los Mejores Hoteles en Cancún para Viajar en Familia</a>
+              <div class="fecha">11 May 2018</div>
+              <a class="imgb" href="https://kooningtravel.com/blog/Los-Mejores-Hoteles-en-Cancun-para-Viajar-en-Familia"><img class="img" src="https://kooningtravel.com/img/blog/chicos/miniatura.png" alt="Cancún, Quintana Roo" /> </a>
+              <p class="info" >Viajar en familia es una de las experiencias más reconfortantes de la vida, olvidarse de la cotidianidad y disponer cuerpo, mente y alma a la aventura es algo que indudablemente debemos hacer al menos una vez al año y que mejor lugar para hacerlo que Cancún.</p>
+             <a class="cult" href="https://kooningtravel.com/blog/Los-Mejores-Hoteles-en-Cancun-para-Viajar-en-Familia">Ver más...</a>         
+        </div>
+        <div class="bl">          
+              <a class="tab1" href="javascript:void(0);">México | Cancún, Quintana Roo..</a>              
+              <a class="tab2" href="https://kooningtravel.com/blog/Los-Mejores-Hoteles-en-Cancun-para-Viajar-en-Familia">Los Mejores Hoteles en Cancún para Viajar en Familia</a>
+              <div class="fecha">11 May 2018</div>
+              <a class="imgb" href="https://kooningtravel.com/blog/Los-Mejores-Hoteles-en-Cancun-para-Viajar-en-Familia"><img class="img" src="https://kooningtravel.com/img/blog/chicos/miniatura.png" alt="Cancún, Quintana Roo" /> </a>
+              <p class="info" >Viajar en familia es una de las experiencias más reconfortantes de la vida, olvidarse de la cotidianidad y disponer cuerpo, mente y alma a la aventura es algo que indudablemente debemos hacer al menos una vez al año y que mejor lugar para hacerlo que Cancún.</p>
+             <a class="cult" href="https://kooningtravel.com/blog/Los-Mejores-Hoteles-en-Cancun-para-Viajar-en-Familia">Ver más...</a>         
+        </div>
+        <div class="bl">          
+              <a class="tab1" href="javascript:void(0);">México | Cancún, Quintana Roo..</a>              
+              <a class="tab2" href="https://kooningtravel.com/blog/Los-Mejores-Hoteles-en-Cancun-para-Viajar-en-Familia">Los Mejores Hoteles en Cancún para Viajar en Familia</a>
+              <div class="fecha">11 May 2018</div>
+              <a class="imgb" href="https://kooningtravel.com/blog/Los-Mejores-Hoteles-en-Cancun-para-Viajar-en-Familia"><img class="img" src="https://kooningtravel.com/img/blog/chicos/miniatura.png" alt="Cancún, Quintana Roo" /> </a>
+              <p class="info" >Viajar en familia es una de las experiencias más reconfortantes de la vida, olvidarse de la cotidianidad y disponer cuerpo, mente y alma a la aventura es algo que indudablemente debemos hacer al menos una vez al año y que mejor lugar para hacerlo que Cancún.</p>
+             <a class="cult" href="https://kooningtravel.com/blog/Los-Mejores-Hoteles-en-Cancun-para-Viajar-en-Familia">Ver más...</a>         
+        </div>
+        <div class="bl">          
+              <a class="tab1" href="javascript:void(0);">México | Cancún, Quintana Roo..</a>              
+              <a class="tab2" href="https://kooningtravel.com/blog/Los-Mejores-Hoteles-en-Cancun-para-Viajar-en-Familia">Los Mejores Hoteles en Cancún para Viajar en Familia</a>
+              <div class="fecha">11 May 2018</div>
+              <a class="imgb" href="https://kooningtravel.com/blog/Los-Mejores-Hoteles-en-Cancun-para-Viajar-en-Familia"><img class="img" src="https://kooningtravel.com/img/blog/chicos/miniatura.png" alt="Cancún, Quintana Roo" /> </a>
+              <p class="info" >Viajar en familia es una de las experiencias más reconfortantes de la vida, olvidarse de la cotidianidad y disponer cuerpo, mente y alma a la aventura es algo que indudablemente debemos hacer al menos una vez al año y que mejor lugar para hacerlo que Cancún.</p>
+             <a class="cult" href="https://kooningtravel.com/blog/Los-Mejores-Hoteles-en-Cancun-para-Viajar-en-Familia">Ver más...</a>         
+        </div>                
+     
+
+   <div class="yuzo_related_post style-1" data-version="5.12.70">
+          <h3>Posts Relacionados</h3>
+            <div class="relatedthumb relatedpost-1977" style="width: 126.5px; float: left; overflow: hidden; height: 182px;"><span class="equalizer-inner" style="display:block;">  
+                <a href="https://kooningtravel.com/blog/premios-platino-v-edicion">
+                <div class="yuzo-img-wrap" >
+                <div class="yuzo-img" style="background:url('/img/blog/chicos/platino.jpg') 50% 50% no-repeat;width: 126.5px;;max-width:100%;height:88px;margin-bottom: 5px;background-size: cover; "></div>
+                </div>
+                <span class="yuzo__text--title" style="font-size:13px;">Premios Platino V Edici&oacute;n.</span>
+                <div><div class="yuzo_views_post yuzo_icon_views yuzo_icon_views__bottom" style="font-size:11px;">Vistas 444</div></div>
+                </a>
+                </span>
+            </div> 
+            <div class="relatedthumb relatedpost-1388" style="width: 126.5px; float: left; overflow: hidden; height: 182px;"><span class="equalizer-inner" style="display:block;">  
+                <a href="https://kooningtravel.com/blog/Puente-de-Dios">
+                <div class="yuzo-img-wrap">
+                <div class="yuzo-img" style="background:url('/img/blog/chicos/puente.jpg') 50% 50% no-repeat;width: 126.5px;;max-width:100%;height:88px;margin-bottom: 5px;background-size: cover; "></div>
+                </div>
+                <span class="yuzo__text--title" style="font-size:13px;">Puente de Dios.</span>
+                <div><div class="yuzo_views_post yuzo_icon_views yuzo_icon_views__bottom" style="font-size:11px;">Vistas 665</div></div>
+                </a>
+                </span>
+            </div>
+            <div class="relatedthumb relatedpost-11888" style="width: 126.5px; float: left; overflow: hidden; height: 182px;"><span class="equalizer-inner" style="display:block;">  
+                <a href="https://kooningtravel.com/blog/Cosas-que-te-haran-regresar-a-Cancun-este-2018">
+                <div class="yuzo-img-wrap " >
+                <div class="yuzo-img" style="background:url('/img/blog/chicos/riviera.jpg') 50% 50% no-repeat;width: 126.5px;;max-width:100%;height:88px;margin-bottom: 5px;background-size: cover; "></div>
+                </div>
+                <span class="yuzo__text--title" style="font-size:13px;">Cosas que te haran regresar a cancun este 2018.</span>
+                <div><div class="yuzo_views_post yuzo_icon_views yuzo_icon_views__bottom" style="font-size:11px;">Vistas 410</div></div>
+                </a>
+                </span>
+            </div>
+            <div class="relatedthumb relatedpost-13275" style="width: 126.5px; float: left; overflow: hidden; height: 182px;"><span class="equalizer-inner" style="display:block;">  
+                <a href="https://kooningtravel.com/blog/Vive-el-Carnaval-Mazatlan-2018">
+                <div class="yuzo-img-wrap" >
+                <div class="yuzo-img" style="background:url('/img/blog/chicos/mazatlan.jpg') 50% 50% no-repeat;width: 126.5px;;max-width:100%;height:88px;margin-bottom: 5px;background-size: cover; "></div>
+                </div>
+                <span class="yuzo__text--title" style="font-size:13px;">Vive el Carnaval Mazatl&aacute;n 2018.</span>
+                <div><div class="yuzo_views_post yuzo_icon_views yuzo_icon_views__bottom" style="font-size:11px;">Vistas 467</div></div>
+                </a>
+                </span>
+            </div>
+
+      </div>
+      
+<div class="blog">
+      <div class="addthis_inline_follow_toolbox"></div>
+      <div class="fb-comments" data-href="https://kooningtravel.com" data-numposts="6"></div>
+</div>
+
+  </div>    
+
+<?php //include(ROOT.'/application/views/commons/bannerblog.php'); ?>
+   
+</div>  
+
+@stop
